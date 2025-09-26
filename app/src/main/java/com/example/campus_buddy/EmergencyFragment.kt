149d0dea5edd52c38a4095c4ad8694699acbffce
Zoe-Hey <zoeheyneke@gmail.com>
2025-09-26 12:46:58 +0200

    in this commit, the action dial function is implemented to dial emergency numbers, also a selection of tasks are implemented for user settings

--- app/src/main/java/com/example/campus_buddy/EmergencyFragment.kt
+++ app/src/main/java/com/example/campus_buddy/EmergencyFragment.kt
@@ -1,59 +1,60 @@
 package com.example.campus_buddy
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.example.campus_buddy.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EmergencyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EmergencyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_emergency, container, false)
+        val view = inflater.inflate(R.layout.fragment_emergency, container, false)
+
+        // dial Ambulance
+        val ambulanceCall: ImageView = view.findViewById(R.id.callAmbulance)
+        ambulanceCall.setOnClickListener {
+            dialPhoneNumber("10177")
+        }
+
+        // dial Police
+        val policeCall: ImageView = view.findViewById(R.id.callPolice)
+        policeCall.setOnClickListener {
+            dialPhoneNumber("10111")
+        }
+
+        // dial Traffic
+        val trafficCall: ImageView = view.findViewById(R.id.callTraffic)
+        trafficCall.setOnClickListener {
+            dialPhoneNumber("0861400800")
+        }
+
+        // dial Campus Security
+        val campusCall: ImageView = view.findViewById(R.id.callCampus)
+        campusCall.setOnClickListener {
+            dialPhoneNumber("0119504099")
+        }
+
+        // dial Zoe test caller
+        val zoeCall: ImageView = view.findViewById(R.id.callZoe)
+        zoeCall.setOnClickListener {
+            dialPhoneNumber("0832226432")
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EmergencyFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EmergencyFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun dialPhoneNumber(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        startActivity(intent)
     }
 }
